590a1480e870e82060cce199a68c800ba0eabee3
Dmytro Mai <dm933@cam.ac.uk>
2025-07-04 00:21:47 +0100

    Made prawns move one square

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,12 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let possibleMoves: Square[] = new Array(0);
+        let currentSquare: Square = board.findPiece(this);
+        
+        let playerDirection: number = this.player == Player.WHITE ? 1 : -1;
+        let oneSquareMove: Square = new Square(currentSquare.row + playerDirection, currentSquare.col);
+        possibleMoves.push(oneSquareMove);
+        return possibleMoves;
     }
 }
