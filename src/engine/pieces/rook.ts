8be52c7d7406303b3791abbcbbcf828a5edcc2cc
Dmytro Mai <dm933@cam.ac.uk>
2025-07-04 01:02:41 +0100

    Made rooks move vertically and horizontally

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,17 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let possibleMoves: Square[] = new Array(0);
+        let currentSquare: Square = board.findPiece(this);
+        
+        //let playerDirection: number = this.player == Player.WHITE ? 1 : -1;
+        for(let i=0; i<=7; i++){
+            if(i!=currentSquare.row) possibleMoves.push(new Square(i,currentSquare.col));
+        }
+        for(let i=0;i<=7; i++){
+            if(i!=currentSquare.col) possibleMoves.push(new Square(currentSquare.row, i));
+        }
+    
+        return possibleMoves;
     }
 }
