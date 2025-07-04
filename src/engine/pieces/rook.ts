008d5123f34cd7431d80cc9061550c0a5018d448
Dmytro Mai <dm933@cam.ac.uk>
2025-07-04 08:16:03 +0100

    Implemented taking other pieces for rook

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -2,6 +2,7 @@ import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
 import Square from '../square';
+import King from './king';
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -22,7 +23,10 @@ export default class Rook extends Piece {
             let newCol:number = col+direction[1];
             while(0<=newRow && newRow<=7 && 0<=newCol && newCol<=7){
                 let possMove = new Square(newRow, newCol);
-                if(board.getPiece(possMove)!==undefined) break;
+                if(board.getPiece(possMove)!==undefined){
+                    if(board.getPiece(possMove)?.player !== this.player && !(board.getPiece(possMove) instanceof King)) possibleMoves.push(possMove);
+                    break;
+                } 
                 possibleMoves.push(possMove);
                 newRow = newRow+direction[0];
                 newCol = newCol+direction[1];
