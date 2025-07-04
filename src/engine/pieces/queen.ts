a3cffcfe324409deeacab89cf012a640f9de01cc
Dmytro Mai <dm933@cam.ac.uk>
2025-07-04 01:34:52 +0100

    Made queens move all directions

--- src/engine/pieces/queen.ts
+++ src/engine/pieces/queen.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Queen extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,25 @@ export default class Queen extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let possibleMoves: Square[] = new Array(0);
+        let currentSquare: Square = board.findPiece(this);
+
+        let row:number = currentSquare.row;
+        let col:number = currentSquare.col;
+        
+        let diagonals:[number,number][] = [[1,1],[-1,-1],[1,-1],[-1,1], [1,0], [0,1], [-1,0],[0,-1]]
+
+        for(const diagonal of diagonals){
+            let newRow:number = row+diagonal[0];
+            let newCol:number = col+diagonal[1];
+            while(0<=newRow && newRow<=7 && 0<=newCol && newCol<=7){
+                let possMove = new Square(newRow, newCol);
+                possibleMoves.push(possMove);
+                newRow = newRow+diagonal[0];
+                newCol = newCol+diagonal[1];
+            }
+        }
+    
+        return possibleMoves;
     }
 }
